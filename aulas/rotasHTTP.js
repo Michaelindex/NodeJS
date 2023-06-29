a2e900192fc4ef732cb429be544fce5d37b65640
Michael Santos <mcialbr123@gmail.com>
2023-06-29 10:25:59 -0300

    Update rotasHTTP.js

--- aulas/rotasHTTP.js
+++ aulas/rotasHTTP.js
@@ -5,7 +5,7 @@ http.createServer((req, res)=>{
     if (rota == '/cadastro'){
         res.end("<h1>Tela de cadastro!!!</h1>")
     } else if (rota == '/consulta'){
-        res.sendFile(__dirname + "aulas/index.html");
+        res.sendFile(__dirname + "index.html");
     } else{
         res.end("<h1>Tela inicial !!!</h1>")
     }
