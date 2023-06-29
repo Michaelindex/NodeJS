492058ddfd7d2a0db7fb88c4b5df09dfe24bffdc
Michael Santos <mcialbr123@gmail.com>
2023-06-29 09:44:53 -0300

    Update rotasHTTP.js

---
+++ aulas/rotasHTTP.js
@@ -0,0 +1,13 @@
+const http=require("http")
+const porta=8180
+http.createServer((req, res)=>{
+    let rota = req.url
+    if (rota == '/cadastro'){
+        res.end("<h1>Tela de cadastro!!!</h1>")
+    } else if (rota == '/consulta'){
+        res.end("<h1>Tela de consulta !!!</h1>")
+    } else{
+        res.end("<h1>Tela inicial !!!</h1>")
+    }
+}) .listen(porta)
+console.log(`Servidor rodando em : http://localhost:${porta} ...`)
